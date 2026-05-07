ab7186b3a10eaeab9c8b0f240b940d4449c7831e
SrSmth <jersonmorenoleon@gmail.com>
2026-05-06 19:47:29 -0500

    Segunda clase Navigation

--- Navigation/src/App.tsx
+++ Navigation/src/App.tsx
@@ -1,12 +1,39 @@
 import React from "react";
-import { Text } from "react-native-gesture-handler";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { NavigationContainer } from "@react-navigation/native";
+import { SafeAreaProvider } from "react-native-safe-area-context";
 
+import { AuthStackParamList } from "./Routes";
+import { LoginPage, RegisterPage } from "./screens";
+
+const Stack = createNativeStackNavigator<AuthStackParamList>();
 
 const App = () => {
     return (
-        <Text>APP</Text>
-    )
-}
+        <SafeAreaProvider>
+            <NavigationContainer>
+                <Stack.Navigator
+                    initialRouteName="Login"
+                    screenOptions={{
+                        headerShown: false,
+                        contentStyle: {
+                            backgroundColor: "#fff",
+                        },
+                    }}
+                >
+                    <Stack.Screen
+                        name="Login"
+                        component={LoginPage}
+                    />
 
+                    <Stack.Screen
+                        name="Register"
+                        component={RegisterPage}
+                    />
+                </Stack.Navigator>
+            </NavigationContainer>
+        </SafeAreaProvider>
+    );
+};
 
 export default App;
--- Navigation/src/components/organisms/LoginForm/LoginForm.tsx
+++ Navigation/src/components/organisms/LoginForm/LoginForm.tsx
@@ -1,10 +1,18 @@
 import React from "react";
+import { Button } from "../../atoms";
+
+interface LoginFormProps {
+    onSubmit : () => void;
+}
 
 
 const LoginForm = () => {
     return (
         <view>
             <text>Formulario login</text>
+            <Button title="Iniciar Sesion" onSubmit={}>
+
+            </Button>
         </view>
     )
 
